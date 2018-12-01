41e87b98c0714dd2dcf983626c0116bd99d1e647
DESKTOP-9D7GSO4\Prashasth K <prashk135@gmail.com>
2018-11-30 21:56:46 -0800

    Added AutoMoveForward autonomous command
    AutoMoveForward integrates PID using magnetic encoders to simply drive forward for an inputted distance

---
+++ src/main/cpp/Commands/AutoMoveForward.cpp
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Commands/AutoMoveForward.h"
+
+AutoMoveForward::AutoMoveForward(double setpoint):
+drivingPID(new WVPIDController(distKp, distKi, distKd, target, false)) {
+  Requires(Robot::m_drive);
+  target = setpoint;
+  // Use Requires() here to declare subsystem dependencies
+  // eg. Requires(Robot::chassis.get());
+}
+
+// Called just before this Command runs the first time
+void AutoMoveForward::Initialize() {
+Robot::m_drive->resetEncoders();
+drivingPID->SetSetPoint(target);
+SetTimeout(5);
+}
+// Called repeatedly when this Command is scheduled to run
+void AutoMoveForward::Execute() {
+power = drivingPID->Tick((Robot::m_drive->getLeftDistance() + Robot::m_drive->getRightDistance())/2);
+Robot::m_drive->tankDrive(0.10 + power, 0.10 + power);
+}
+// Make this return true when this Command no longer needs to run execute()
+bool AutoMoveForward::IsFinished() { 
+if (fabs(drivingPID->GetError()) < 0.05) {
+  return true;
+}
+else return IsTimedOut();
+}
+// Called once after isFinished returns true
+void AutoMoveForward::End() {
+Robot::m_drive->tankDrive(0,0);
+}
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void AutoMoveForward::Interrupted() {}
---
+++ src/main/include/Commands/AutoMoveForward.h
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "WVPIDController.h"
+#include "OI.h"
+#include "Subsystems/DriveTrain.h"
+#include "../Robot.h"
+class AutoMoveForward : public frc::Command {
+ private:
+ double target;
+ double distKp = 0.1;
+ double distKi = 0;
+ double distKd = 0;
+ double power = 0;
+
+ WVPIDController * drivingPID;
+
+ public:
+  AutoMoveForward(double setpoint);
+  void Initialize() override;
+  void Execute() override;
+  bool IsFinished() override;
+  void End() override;
+  void Interrupted() override;
+};
